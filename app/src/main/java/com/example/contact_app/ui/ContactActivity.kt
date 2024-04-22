0815f4cf8e552719471de5c4e9cc861e04b40956
rlaxodud214 <rlaxodud214@naver.com>
2024-04-22 19:16:07 +0900

    initial project

---
+++ app/src/main/java/com/example/contact_app/ui/ContactActivity.kt
@@ -0,0 +1,52 @@
+package com.example.contact_app.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.contact_app.R
+import com.example.contact_app.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+
+class ContactActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        // Default로 ContactList Fragment가 뜨도록 추가함
+        setFragment(ContactListFragment())
+
+        // 프레임 레이아웃에 Fragment를 띄운다.
+        with(binding) {
+            tlItems.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    tab?.connectFragment()
+                }
+
+                override fun onTabUnselected(p0: TabLayout.Tab?) {}
+                override fun onTabReselected(p0: TabLayout.Tab?) {}
+            })
+
+        }
+    }
+
+    private fun TabLayout.Tab.connectFragment() = with(binding) {
+        when (this@connectFragment.position) {
+            0 -> setFragment(ContactListFragment())
+            1 -> setFragment(MyPageFragment())
+        }
+    }
+
+    private fun setFragment(frag: Fragment) {
+        // SFM : 사용자의 상호작용에 응답해 Fragment를 add 또는 remove 할 수 있는 class
+        supportFragmentManager.commit {
+            replace(R.id.fl_frag, frag)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+    }
+}
