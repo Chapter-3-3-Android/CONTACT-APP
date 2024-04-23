ee8a949e161c4f768215aaed0f76a0d1bbcf60f1
TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>
2024-04-23 12:14:47 +0900

    feat: Viewpager2를 이용하여 Fragment 간의 swipe 기능 구현 (#3)
    viewPager2와 TabLayout 연결

---
+++ app/src/main/java/com/example/contact_app/ui/adapter/ViewPagerFragmentAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.contact_app.ui.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.contact_app.ui.ContactListFragment
+import com.example.contact_app.ui.MyPageFragment
+
+class ViewPagerFragmentAdapter(
+    fragmentActivity: FragmentActivity
+): FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount() = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> ContactListFragment()
+            1 -> MyPageFragment()
+            else -> throw RuntimeException("현재 Fragment는 2개 입니다.")
+        }
+    }
+}
