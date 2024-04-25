bd94245e8c13a6a63f37667ee3a71be9db2ee8a5
Kang <77070839+BanDalKang@users.noreply.github.com>
2024-04-25 13:27:00 +0900

    feat: 스케줄 추가 다이얼로그 구현 (#28)
    * fix: 병합 오류 수정
    
    * add: strings.xml
    
    * refactor: AddContactDialogFragment UI 수정
    
    - EditText hint 문구 변경
    - strings.xml 수정
    
    * feat: 입력받은 데이터 유효성 검사를 위한 정규식 생성
    
    * feat: AddContactDialogFragment 새로운 연락처 추가 다이얼로그 구현
    
    프로필 이미지, 이름, 전화번호, 이메일을 입력받아 유저 목록에 추가
    
    * fix: 잘못 쓴 변수 수정
    
    nameEnable -> emailEnable
    
    * feat: 스케줄 추가 다이얼로그 구현
    
    AddScheduleDialogFragment 구현
    
    * fix: AddContactDialogFragment 안 뜨는 오류 해결 & UI 수정
    
    * feat: 스케줄 추가 다이얼로그 구현
    
    * feat: 유효한 입력이 모두 확인되면 Save 버튼 색상 변경
    
    사진 선택 안해도 Save 가능하도록 변경
    
    * feat: 유효한 입력이 확인되면 Save버튼 색상 변경

--- app/src/main/java/com/example/contact_app/data/model/Schedule.kt
+++ app/src/main/java/com/example/contact_app/data/model/Schedule.kt
@@ -1,7 +1,5 @@
 package com.example.contact_app.data.model
 
-import java.time.LocalTime
-
 data class Schedule(
     val name: String,
     val date: String,
--- app/src/main/java/com/example/contact_app/extension/ValidExtension.kt
+++ app/src/main/java/com/example/contact_app/extension/ValidExtension.kt
@@ -10,4 +10,12 @@ object ValidExtension {
     //이메일 정규 표현식
     fun String.validEmail() =
         Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}\$").matches(this)
+
+    // 영어나 한글이나 숫자로 1~20자 사이
+    fun String.validDate() =
+        Regex("^[A-Za-zㄱ-ㅣ가-힣0-9]{1,20}\$").matches(this)
+
+    // 리마인드 시간 (분 단위, 3자리까지)
+    fun String.validRemindTime() =
+        Regex("^[1-9]\\d{0,2}\$").matches(this)
 }
