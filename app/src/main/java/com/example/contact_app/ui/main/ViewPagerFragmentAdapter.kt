724d26c5314bba2762d7622c755cfa248a5a3896
rlaxodud214 <rlaxodud214@naver.com>
2024-04-25 16:58:53 +0900

    fix: 잘못된 Fragment를 띄우던 오류 수정

--- app/src/main/java/com/example/contact_app/ui/main/ViewPagerFragmentAdapter.kt
+++ app/src/main/java/com/example/contact_app/ui/main/ViewPagerFragmentAdapter.kt
@@ -4,6 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.contact_app.ui.contact.ContactDetailFragment
+import com.example.contact_app.ui.contact.MyPageFragment
 import com.example.contact_app.ui.contactList.ContactListFragment
 
 class ViewPagerFragmentAdapter(
@@ -17,7 +18,7 @@ class ViewPagerFragmentAdapter(
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> contactListFragment
-            1 -> ContactDetailFragment()
+            1 -> MyPageFragment()
             else -> throw RuntimeException("현재 Fragment는 2개 입니다.")
         }
     }
