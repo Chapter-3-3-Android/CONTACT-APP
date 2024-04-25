1ac30c0918e1674b380d8bb82c147676f97c2953
cococo35 <161271441+cococo35@users.noreply.github.com>
2024-04-25 13:22:25 +0900

    feat: Contact Detail Fragment 기능 구현 및 myPage Fragment와 병합 (#26)
    * feat: 메뉴바 클릭시 수정, 삭제 가능하도록 하기(MyPage에선 수정 삭제 둘다, Detail에선 삭제만)
    
    * feat: MyPage에서는 전화, 문자, 영상통화 ImageView 안 보이게 적용
    
    * feat: 전화, 문자 아이콘 누르면 기본 전화 앱, 기본 문자 앱 연결
    
    * feat: ContactDetail, MyPage에서 정보 표시할 때 Bundle 이용하여 데이터 전달받아서 사용
    
    * feat: 전화번호, 이메일 Copy 기능 구현, Copy 할때 toast까지
    - Account Linked에 TIL 블로그 링크, github 링크 연결
    
    * add: UserProvider > random profileImage
    
    * add: UserProvider > random blogLink, gitLink
    
    ---------
    
    Co-authored-by: seotoung19920921 <meba22@naver.com>
    Co-authored-by: rlaxodud214 <rlaxodud214@naver.com>
    Co-authored-by: TaeYoung Kim <52482206+rlaxodud214@users.noreply.github.com>

--- app/src/main/java/com/example/contact_app/data/UserProvider.kt
+++ app/src/main/java/com/example/contact_app/data/UserProvider.kt
@@ -15,15 +15,15 @@ object UserProvider {
         _users.add(user)
     }
 
-    //MyPageFragment, ContactDetailFragment 수정 기능 구현중
-    fun modifyUser() {
-        _users
-    }
+    // MyPageFragment, ContactDetailFragment 수정 기능
+//    fun modifyUser() {
+//        _users
+//    }
 
-    //MyPageFragment, ContactDetailFragment 삭제 기능 구현
-    fun deleteUser(position: Int) {
-        _users.removeAt(position)
-    }
+    // MyPageFragment, ContactDetailFragment 삭제 기능
+//    fun deleteUser(position: Int) {
+//        _users.removeAt(position)
+//    }
 
     fun switchFavoriteByUser(index: Int) {
         _users[index] = _users[index].copy(
@@ -35,18 +35,42 @@ object UserProvider {
         val users = mutableListOf<User>()
         val firstNames = listOf("James", "Mary", "John", "Patricia", "Robert", "Jennifer", "Michael", "Linda", "William", "Elizabeth")
         val lastNames = listOf("Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor")
+        val images = listOf(
+            R.drawable.profile1, R.drawable.profile2, R.drawable.profile3, R.drawable.profile4, R.drawable.profile5,
+            R.drawable.profile6, R.drawable.profile7,R.drawable.profile8, R.drawable.profile9, R.drawable.profile10
+        )
+        val blogLinks = listOf(
+            "https://rlaxodud214.tistory.com/",
+            "https://muk-clouds.tistory.com/",
+            "https://nochfm0513.tistory.com/",
+            "https://alwayszzurie.tistory.com/",
+            "https://jihyung1997.tistory.com/"
+        )
+        val gitLinks = listOf(
+            "https://github.com/rlaxodud214",
+            "https://github.com/BanDalKang",
+            "https://github.com/seoyoung19920921",
+            "https://github.com/cococo35",
+            "https://github.com/jihyung97"
+        )
 
         for (i in 1..30) {
             val firstName = firstNames.random()
             val lastName = lastNames.random()
             val phoneNumber = "+233 ${(1000..9999).random()} ${(10..99).random()} ${(100..999).random()}"
             val email = "${firstName.lowercase()}${lastName.lowercase()}${i}@example.com"
+            val profileImage = images.random()
+            val blogLink = blogLinks.random()
+            val gitLink = gitLinks.random()
 
             val user = User(
                 name = "$firstName $lastName",
                 phoneNumber = phoneNumber,
-                profileImage = Image.ImageDrawable(R.drawable.ic_user),
+                profileImage = Image.ImageDrawable(profileImage),
                 email = email
+                //적용안되는 문제 해결해야됨
+                //blogLink = blogLink
+                //githubLink = gitLink
             )
             users.add(user)
         }
